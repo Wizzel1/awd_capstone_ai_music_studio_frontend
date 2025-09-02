46ba6064b2e79feed6fc7f6777eef6ec0bb834f9
Wizzel1 <r_christian@gmx.de>
2025-09-02 10:40:46 +0200

    Create ProjectsService

---
+++ lib/services/projectsService.ts
@@ -0,0 +1,30 @@
+export type Project = {
+  id?: string;
+  name: string;
+  description?: string;
+};
+
+export class ProjectsService {
+  private static baseUrl = process.env.NEXT_PUBLIC_API_URL;
+
+  static async createProject(project: Project) {
+    console.log("Creating project:", project);
+    return fetch(`${this.baseUrl}/api/v1/projects`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    });
+  }
+
+  static async getProjects(): Promise<Project[]> {
+    const response = await fetch(`${this.baseUrl}/api/v1/projects`);
+    return response.json();
+  }
+
+  static async getProject(id: string): Promise<Project> {
+    const response = await fetch(`${this.baseUrl}/api/v1/projects/${id}`);
+    return response.json();
+  }
+}
