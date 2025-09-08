ca4b9c3d161f84b1a478b36909f1f7357c7f508f
Wizzel1 <r_christian@gmx.de>
2025-09-08 16:28:18 +0200

    Update styling

--- app/projects/[id]/TaskFileCard.tsx
+++ app/projects/[id]/TaskFileCard.tsx
@@ -4,7 +4,7 @@ export default function TaskFileCard({ task }: { task: Task }) {
   const { status, error } = task;
 
   return (
-    <div>
+    <div className="relative aspect-square rounded-lg border-2 cursor-pointer transition-all overflow-hiddenborder-zinc-200 hover:border-zinc-300 hover:shadow-sm">
       <h1>{status}</h1>
       <p>{error}</p>
     </div>
