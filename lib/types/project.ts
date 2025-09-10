fabb62cfb53c9663de20bde743360169402638b8
Wizzel1 <r_christian@gmx.de>
2025-09-10 11:11:45 +0200

    Move asset schema to own file

--- lib/types/project.ts
+++ lib/types/project.ts
@@ -1,23 +1,6 @@
 import { z } from "zod";
+import { assetSchema } from "./asset";
 import { taskSchema } from "./task";
-
-const metadataSchema = z.object({
-  size: z.number(),
-  duration: z.number().optional(),
-});
-
-export const assetSchema = z.object({
-  id: z.uuid(),
-  originalName: z.string(),
-  storageName: z.string(),
-  downloadUrl: z.string(),
-  createdAt: z.string(),
-  updatedAt: z.string(),
-  format: z.enum(["audio", "image", "video"]),
-  metadata: metadataSchema.optional(),
-});
-
-export type Asset = z.infer<typeof assetSchema>;
 
 export const projectSchema = z.object({
   id: z.string(),
